4b7dcfa6d04189cd424d6f7920d143dbd69ba2c6
mike <paagrio2005@yandex.ru>
2023-10-08 02:20:59 +0300

    auth0

--- src/main/java/com/example/oauthSpring/controller/HomeController.java
+++ src/main/java/com/example/oauthSpring/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.example.oauthSpring.controller;
 
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-
 import java.io.IOException;
 import java.util.Map;
 
@@ -20,9 +21,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
-        System.out.println("YA TUT");
-        System.out.println(principal.getClaims());
-        System.out.println(model.toString());
         if (principal != null) {
             model.addAttribute("profile", principal.getClaims());
         }
@@ -32,8 +30,16 @@ public class HomeController {
 
     @GetMapping("/secured")
     public String secured() {
-        return "Hello, Secured!";
+
+//        HttpResponse<String> response = Unirest.post("https://{yourDomain}/oauth/token")
+//                .header("content-type", "application/x-www-form-urlencoded")
+//                .body("grant_type=client_credentials&client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&audience=YOUR_API_IDENTIFIER")
+//                .asString();
+
+
+        return "secured";
     }
+
 
     @GetMapping("/secured1")
     public String secured1(@AuthenticationPrincipal OidcUser oidcUser) {
@@ -55,5 +61,12 @@ public class HomeController {
         System.out.println(req.toString());
         res.sendRedirect("/login/oauth2/code/auth0");
     }
+
+    @GetMapping("/login")
+    ResponseEntity<Map<String, Object>> login(@RequestHeader("Authorization") String auth) {
+        System.out.println(auth);
+        return null;
+    }
+
 
 }
